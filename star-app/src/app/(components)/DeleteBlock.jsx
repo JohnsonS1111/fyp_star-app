29d4b70ae4b9fe8ad5091273574c2c78d486093c
Johnson Shogbaike <g00389668@atu.ie>
2024-02-07 13:54:39 +0000

    minor changes

--- star-app/src/app/(components)/DeleteBlock.jsx
+++ star-app/src/app/(components)/DeleteBlock.jsx
@@ -1,10 +1,10 @@
-import { faX } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const DeleteBlock = () => {
   return (
-    <FontAwesomeIcon icon={faX} className="text-red-400 hover:text-red-200" />
+    <FontAwesomeIcon icon={faTrash} className="text-grey-500 hover:text-red-600" />
   );
 };
 
