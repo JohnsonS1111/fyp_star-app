e45ee78e438022bd220debe7e513a3962d2d7f44
Johnson Shogbaike <g00389668@atu.ie>
2024-04-16 22:37:42 +0100

    added to user schema and deleted timetable stuff

--- star-app/src/models/User.js
+++ star-app/src/models/User.js
@@ -4,26 +4,32 @@ mongoose.connect(process.env.MONGODB_URI);
 
 const userSchema = new Schema(
   {
-    firstname: {
+    username: {
       type: String,
-      required: true,
-    },
-    surname: {
-      type: String,
-      required: true,
+      required: [true, "Please provide a username"],
+      unique: true,
     },
     email: {
       type: String,
-      required: true,
+      required: [true, "Please provide an email"],
+      unique: true,
     },
     password: {
       type: String,
-      required: true,
+      required: [true, "Please provide a password"],
     },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpire: Date,
+    verifyToken: String,
+    verifyTokenExpire: Date,
   },
   {
     timestamps: true,
   }
 );
-const User = mongoose.models.User || mongoose.model("User", userSchema);
+const User = mongoose.models.Users || mongoose.model("User", userSchema);
 export default User;
